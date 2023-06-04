bca45fd2ab0bdf7eb519e578a07ee31508893397
tri <vobien2096@gmail.com>
2023-06-04 09:49:49 +0700

    Define load static files

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+// Load static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 // HTTP logger
 app.use(morgan('combined'))
 
