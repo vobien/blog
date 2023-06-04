5168e074a4a4e98e7ec3649d00063a249aa05865
tri <vobien2096@gmail.com>
2023-06-04 16:35:50 +0700

    Refactor MVC pattern - router & controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+class NewsController {
+
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET] /news/:newId
+    detail(req, res) {
+        res.send('News Details');
+    }
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const engine = require('express-handlebars').engine;
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express()
 const port = 3000
 
@@ -25,25 +27,8 @@ app.engine('.hbs', engine({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-
-  console.log(req.body);
-
-  res.send('');
-})
+// Load routes
+routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
